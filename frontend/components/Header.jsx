c128694489390abfb99a3ff286e64b3aa1fe982e
majid <majidkawouk@gmail.com>
2025-09-23 15:18:36 +0300

    header

--- frontend/components/Header.jsx
+++ frontend/components/Header.jsx
@@ -5,10 +5,10 @@ import Link from "next/link";
 export default function Header() {
   return (
     <div className="border-b p-5 border-gray-300 dark:border-gray-300 pb-4 w-full flex items-center justify-between ">
-      <div className="flex items-center pl-5">
+      <Link className="flex items-center pl-5" href="/">
         <Sparkles className="text-cyan-400 w-8 h-8" />
         <span className="text-2xl font-bold ml-2">Image Optimization</span>
-      </div>
+      </Link>
       <Link className="font-medium" href="/editor">
         <div className="bg-[#06B6D4] font-semibold  text-white px-4 cursor-pointer py-2 rounded-lg">Get started</div>
       </Link>
